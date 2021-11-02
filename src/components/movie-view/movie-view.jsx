94801de8f84468153986cc26db022f46e56ed7b2
keefeharris <keefeharrisjr@gmail.com>
2021-11-01 23:22:39 -0400

    Create movie-view.jsx

---
+++ src/components/movie-view/movie-view.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+//testing for github pages
+export class MovieView extends React.Component {
+
+    render() {
+        const { movie, onBackClick } = this.props;
+
+        return (
+            <div className="movie-view">
+                <div className="movie-poster">
+                    <img src={movie.ImagePath} />
+                </div>
+                <div className="movie-title">
+                    <span className="label">Title: </span>
+                    <span className="value">{movie.Title}</span>
+                </div>
+                <div className="movie-description">
+                    <span className="label">Description: </span>
+                    <span className="value">{movie.Description}</span>
+                </div>
+                <div className="genre-title">
+                    <span className="label">Genre: </span>
+                    <span className="value">{movie.Genre.Name}</span>
+                </div>
+                <div className="genre-description">
+                    <span className="label">Description: </span>
+                    <span className="value">{movie.Genre.Description}</span>
+                </div>
+                <div className="director-name">
+                    <span className="label">Director: </span>
+                    <span className="value">{movie.Director.Name}</span>
+                </div>
+                <div className="director-bio">
+                    <span className="label">Bio: </span>
+                    <span className="value">{movie.Director.Bio}</span>
+                </div>
+                <button onClick={() => { onBackClick(null); }}>Back</button>
+
+            </div>
+        );
+    }
+}
+
+MovieView.propTypes = {
+    movie: PropTypes.shape({
+        Title: PropTypes.string.isRequired,
+        Description: PropTypes.string.isRequired,
+        ImagePath: PropTypes.string.isRequired,
+        Genre: PropTypes.shape({
+            Name: PropTypes.string.isRequired,
+            Description: PropTypes.string.isRequired
+        }),
+        Director: PropTypes.shape({
+            Name: PropTypes.string.isRequired,
+            Bio: PropTypes.string.isRequired
+        }),
+    }).isRequired
+}
